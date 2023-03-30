062b448ee41a82756830807365897fae1af96946
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-03-30 11:55:24 +0530

    Added Password to MySQL Tools

--- portfolio-schema.sql
+++ portfolio-schema.sql
@@ -8,12 +8,12 @@ USE portfolio;
 
 CREATE TABLE user ( -- Done
   user_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  email VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL UNIQUE,
   passwrd VARCHAR(50) NOT NULL,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NOT NULL,
   phone_number BIGINT UNSIGNED,
-  pan_number BIGINT UNSIGNED,
+  pan_number BIGINT UNSIGNED NOT NULL,
   --------------------------
   PRIMARY KEY  (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
