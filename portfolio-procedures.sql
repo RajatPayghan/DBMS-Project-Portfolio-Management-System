587de94a576a137f7097a91d1adbb8d8b0f3f0e0
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-04-04 21:11:36 +0530

    Updated

--- portfolio-procedures.sql
+++ portfolio-procedures.sql
@@ -1,12 +1,16 @@
---@block COMMENT Procedure for total return
-DELIMITER $$
-CREATE PROCEDURE getTotalReturn(IN u_id BIGINT)
+--@block COMMENT Procedures : 
+DELIMITER //
+CREATE PROCEDURE viewAllInvestments(IN input_user BIGINT UNSIGNED)
 BEGIN
-	SELECT 
-		SUM((inv_in.quantity)*(investment.current_price - invests_in.entry_price))
-	FROM user AS u
-  INNER JOIN invests_in AS inv_in ON u.user_id = inv_in.user_id
-  INNER JOIN invests_in AS inv_in ON u.user_id = inv_in.user_id
-  WHERE u.user_id = u_id;   
-END$$
+  SELECT 
+    invests_in.symbol AS Symbol,
+    investment.name AS Symbol_Name,
+    invests_in.quantity AS Quantity,
+    invests_in.entry_price AS Entry_Price,
+    investment.current_price AS Current_Price,
+    invests_in.quantity*(investment.current_price-invests_in.entry_price) AS Invst_return
+  FROM invests_in
+  INNER JOIN investment USING (symbol)
+  WHERE invests_in.user_id = input_user;
+END //
 DELIMITER ;
--- portfolio-schema.sql
+++ portfolio-schema.sql
@@ -83,20 +83,3 @@ CREATE TABLE financial_info( -- Running
   PRIMARY KEY (date_of_data) 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
---@block COMMENT Procedures : 
-DELIMITER //
-CREATE PROCEDURE viewAllInvestments(IN input_user BIGINT UNSIGNED)
-BEGIN
-  SELECT 
-    invests_in.symbol AS Symbol,
-    investment.name AS Symbol_Name,
-    invests_in.quantity AS Quantity,
-    invests_in.entry_price AS Entry_Price,
-    investment.current_price AS Current_Price,
-    invests_in.quantity*(investment.current_price-invests_in.entry_price) AS Invst_return
-  FROM invests_in
-  INNER JOIN investment USING (symbol)
-  WHERE invests_in.user_id = input_user;
-END //
-DELIMITER ;
