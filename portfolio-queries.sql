ef7293f65603f44ead5de72ec7fa3b7aa5a24b78
Rajat Payghan <93655125+RajatPayghan@users.noreply.github.com>
2023-04-11 18:46:59 +0530

    Update portfolio-queries.sql

--- portfolio-queries.sql
+++ portfolio-queries.sql
@@ -1,7 +1,7 @@
 
-/* Queries asked */
+/* Queries asked 1 to 4*/
 
-/* This table is already Created
+-- This table is already Created
 CREATE TABLE invests_in ( -- Running
   user_id BIGINT UNSIGNED NOT NULL,
   symbol VARCHAR(10) NOT NULL,
@@ -19,9 +19,8 @@ CREATE TABLE invests_in ( -- Running
     ON DELETE RESTRICT 
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-*/
 
-/*
+
 CREATE TABLE investment ( -- Running
   id VARCHAR(10) NOT NULL,
   name VARCHAR(45) NOT NULL,
@@ -32,9 +31,9 @@ CREATE TABLE investment ( -- Running
   risk_level DOUBLE (5,2),
   PRIMARY KEY  (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-*/
 
-/*
+
+
 CREATE TABLE market_data ( -- Running
   symbol VARCHAR(10) NOT NULL,
   on_date TIMESTAMP NOT NULL,
@@ -50,9 +49,9 @@ CREATE TABLE market_data ( -- Running
     ON DELETE RESTRICT 
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-*/
 
-/*
+
+
 CREATE TABLE financial_info( -- Running
   date_of_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   interest DOUBLE(5,2),
@@ -60,27 +59,27 @@ CREATE TABLE financial_info( -- Running
   gdp DOUBLE(5,2),
   PRIMARY KEY (date_of_data) 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-*/
 
---@block Query 5
+
+--@block Query 5 : Insert a new investment into the investments table.
 INSERT INTO investment VALUES ('INFY','Infosys Pvt Ltd','Equity',1244.76,-3.02,-20.44,-0.16);
 INSERT INTO invests_in VALUES (1,'INFY',30,200,'2023-04-10 02:22:49',20);
 
 -- To check : select * from invests_in where symbol='INFY';
 
---@block Query 6
+--@block Query 6 : Update the number of shares held for an investment in the investments table.
 UPDATE invests_in
   SET quantity = 50 
   WHERE symbol = 'INFY' AND user_id = '1';
 
---@block Query 7
+--@block Query 7 : Delete an investment from the investments table.
 DELETE FROM investment WHERE symbol = 'INFY';
 
---@block Query 8
+--@block Query 8 : Join the investments table with the performance metrics table to retrieve the total return for each investment.
 SELECT * from viewallinvestments
 WHERE user_id = 1;
 
---@block Query 9
+--@block Query 9 : Join the investments table with the market data table to retrieve the stock prices for a particular date.
 SELECT 
   market_data.on_date,
   investment.name,
@@ -97,14 +96,14 @@ WHERE
   on_date = '2023-04-03 '
 ;
 
---@block Query 10
+--@block Query 10 : Group the investments by type and retrieve the average annualized return for each type.
 SELECT 
   investment.type as 'Investment Type',
   AVG(annualized_return) as 'Average Annualized Return'
 FROM investment
 GROUP BY type;
 
---@block Query 11
+--@block Query 11 : Filter the investments by risk level and retrieve the top-performing investments.
 SELECT
  investment.name,
  investment.risk_level,
@@ -112,7 +111,7 @@ SELECT
 FROM investment
 ORDER BY risk_level ASC;
 
---@block Query 12
+--@block Query 12 : Calculate the total value of all investments based on the number of shares held and the current stock prices from the market data table.
 SELECT 
   symbol,
   SUM(Invst_return) AS Total_Return
@@ -125,7 +124,7 @@ SELECT
  total_return
 FROM investment;
 
---@block Query 13
+--@block Query 13 : Calculate the portfolio’s overall annualized return based on the investments’ individual returns and the number of shares held.
 SELECT SUM(percent_weight*annualized_return)/100 as 'Percent Annualized Return'
 FROM investment
 INNER JOIN invests_in USING (symbol)
@@ -137,7 +136,7 @@ GROUP BY user_id;
 
 
 
---@block Query 15
+--@block Query 15 : Retrieve the most recent inflation rate from the other financial information table.
 SELECT 
   date_of_data as 'Most Recent Date',
   inflation as 'Inflation Rate'
@@ -145,13 +144,13 @@ FROM financial_info
 ORDER BY date_of_data DESC
 LIMIT 1;
 
---@block Query 16
+--@block Query 16 : Filter the market data table by date range and retrieve the stock prices for a particular investment.
 SELECT * FROM market_data
 WHERE symbol = 'batns'
 ORDER BY on_date DESC;
 
 
---@block Query 17
+--@block Query 17 : Calculate the percentage change in stock prices for a particular investment between two dates.
 DELIMITER $$
   DROP FUNCTION IF EXISTS que_17; 
   CREATE FUNCTION que_17(input_start_date DATE, input_end_date DATE, input_symbol VARCHAR(50)) returns DOUBLE(10,2)
@@ -174,19 +173,19 @@ DELIMITER ;
 SELECT que_17('2023-04-02','2023-04-03','mpkbc');
 
 
---@block Query 18
+--@block Query 18 : Calculate the volatility of an investment’s returns using the performance metrics table.
 SELECT STDDEV(close)/100 as Volatility
 from market_data
 where symbol = 'batns'
 GROUP BY symbol;
 
 
---@block Query 19
+--@block Query 19 : Retrieve the top-performing investments based on annualized return and risk level.
 SELECT * from investment
 ORDER BY annualized_return/risk_level DESC;
 
 
---@block Query 20
+--@block Query 20 : Group the investments by type and retrieve the total number of shares held for each type.
 SELECT 
   investment.type as 'Investment Type',
   SUM(invests_in.quantity) as Quantity
